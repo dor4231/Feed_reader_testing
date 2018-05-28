f222e1f2070d792bea6496d255e3c2195ae5af5b
Dor Bar <dor4231@gmail.com>
2018-05-28 22:15:56 +0300

    test: loadFeed() is running and retrieves information

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -58,8 +58,7 @@ $(function() {
             expect(body.classList).toContain('menu-hidden');
         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
+         /* visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
@@ -69,20 +68,35 @@ $(function() {
             var burgerIcon = document.querySelector('.menu-icon-link');
 
             burgerIcon.click();
-            expect(body.classList).not.toBe('menu-hidden');
+            expect(body.classList).not.toContain('menu-hidden');
             burgerIcon.click();
+            expect(body.classList).toContain('menu-hidden');
         });
 
     });
 
     describe("Initial Entries", function() {
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+        /* Test that ensures when the loadFeed function is called
+         * and completes its work, there is at least a single
+         * .entry element within the .feed container. Remember,
+         * loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done()
+         * function.
          */
+        it('loadFeed function called and function well', function() {
+            var feed = document.querySelector('.feed');
+            var numOfEnteries = feed.getElementsByClassName('entry').length;
+
+            expect(numOfEnteries).toBeGreaterThan(0);
+        });
+
     });
 
     describe("New Feed Selection", function() {
